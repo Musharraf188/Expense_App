a5cdf083148d33a752a96c007e929eba1110ab97
Musharraf <musharrafsayyad1@gmail.com>
2023-03-03 12:28:11 +0530

    Tracker Started

---
+++ expensetraker/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+// First components for Render List data..
+
+const ExpenseItem =() =>{
+    return <div>
+        <div>March 28th 2022</div>
+        <div>
+            <h2>Car Insuranse</h2>
+            <div>$213.75</div>
+        </div>
+    </div>
+}
+
+export default ExpenseItem;
