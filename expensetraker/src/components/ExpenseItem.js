fb3252d888b2b4249ae7802183f2e411abfcea35
Musharraf <musharrafsayyad1@gmail.com>
2023-03-03 12:58:27 +0530

    Expenseitem Created

--- expensetraker/src/components/ExpenseItem.js
+++ expensetraker/src/components/ExpenseItem.js
@@ -1,11 +1,20 @@
 // First components for Render List data..
+import "./ExpenseItem.css";
+const ExpenseItem =(props) =>{
+const month= props.date.toLocaleString('en-US', {month : 'long'});
+const day= props.date.toLocaleString('en-US', {day : '2-digit'});
+const year= props.date.getFullYear();
 
-const ExpenseItem =() =>{
+// const month = props.date.toLocaleString('en-US', { month: 'long' })
+// const day = props.date.toLocaleString('en-US', { day: '2-digit' })
+// const year = props.date.getFullYear()
     return <div>
-        <div>March 28th 2022</div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
         <div>
-            <h2>Car Insuranse</h2>
-            <div>$213.75</div>
+            <h2>{props.title}</h2>
+            <div>${props.amount}</div>
         </div>
     </div>
 }
