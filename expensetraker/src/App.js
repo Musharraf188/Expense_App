5f00cc486a0395dfaace50e6ece68f7186da8bca
Musharraf <musharrafsayyad1@gmail.com>
2023-03-06 20:06:25 +0530

    ExpenseFilter

--- expensetraker/src/App.js
+++ expensetraker/src/App.js
@@ -1,38 +1,49 @@
 // Root Compponent for rendering all custom components..
 
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpenses from "./components/NewExpenses/NewExpense";
 
 
 
 const App = () => {
-  const expenses = [
+  const DUMMY_EXPENSES= [
     {
+      id:1,
       date: new Date(2023, 3, 12),
       title: "Car Insurance",
       amount: 234.87
     },
 
     {
+      id:2,
       date: new Date(2023, 1, 30),
       title: "Home Expenses",
       amount: 234.87
     },
 
     {
+      id:3,
       date: new Date(2022, 9, 11),
       title: "Bike Purchase",
       amount: 234.87
     },
 
     {
+      id:4,
       date: new Date(2022, 4, 19),
       title: "Self Expenses",
       amount: 234.87
     },
   ]
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const saveData =(userInput)=>{
+setExpenses(dummyExpenses=>{
+  return [userInput, ...dummyExpenses];
+})
+  }
   return <div>
-    <NewExpenses />
+    <NewExpenses onAddItem={saveData} />
 <Expenses item={expenses} />
   </div> 
 }
--- expensetraker/src/components/Expenses/ExpenseForm.js
+++ expensetraker/src/components/Expenses/ExpenseForm.js
@@ -1,22 +1,57 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
-    return <form>
+const ExpenseForm = (props) => {
+const [userInput, setUserInput] = useState({
+    expenseTitle:"",
+    expenseAmount:"",
+    expenseDate:""
+})
+    function titleHandler(event){
+setUserInput((prevValue)=>{
+    return {...prevValue, 
+        expenseTitle:event.target.value}
+})
+    }
+    function amountHandler(event){
+
+setUserInput((prevValue)=>{
+    return {...prevValue, expenseAmount:event.target.value}
+})
+    }
+    function dateHandler(event){
+setUserInput((prevValue)=>{
+    return {...prevValue, expenseDate:event.target.value}
+})
+    }
+    const submitHandler =(event)=>{
+event.preventDefault();
+const expenseData ={
+    title:userInput.expenseTitle,
+    amount:userInput.expenseAmount,
+    date: new Date(userInput.expenseDate)
+}
+props.onSaveExpenseData(expenseData)
+setUserInput({expenseTitle:"",expenseAmount:"", expenseDate:""})
+
+
+    }
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control label">
                 <label>Title</label>
-                <input type="text"></input>
+                <input type="text" value={userInput.expenseTitle} onChange={titleHandler}></input>
             </div>
             <div className="new-expense__control label">
                 <label>Amount</label>
-                <input type="text"></input>
+                <input type="text" value={userInput.expenseAmount} onChange={amountHandler}></input>
             </div>
             <div className="new-expense__control label">
                 <label>Date</label>
-                <input type="text"></input>
+                <input type="date" value={userInput.expenseDate} onChange={dateHandler}></input>
             </div>
         </div >
-        <div className="new-expense__actions ">
+        <div className="new-expense__actions">
         <button type="submit">Add Expense</button>
         </div>
     </form>
