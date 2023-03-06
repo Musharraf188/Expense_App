bd553de0d0a8a78d5b963f9271683e7b48fb95f2
Musharraf <musharrafsayyad1@gmail.com>
2023-03-06 11:21:12 +0530

    NewExpenses Form is created

--- expensetraker/src/components/Expenses/Expenses.js
+++ expensetraker/src/components/Expenses/Expenses.js
@@ -1,13 +1,15 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
 
-const Expenses =(props)=>{
-    return <Card className="expenses">
+const Expenses = (props) => {
+  return <Card className="expenses">
+    <h1>Let's Start</h1>
     <ExpenseItem date={props.item[0].date} title={props.item[0].title} amount={props.item[0].amount} />
     <ExpenseItem date={props.item[1].date} title={props.item[1].title} amount={props.item[1].amount} />
     <ExpenseItem date={props.item[2].date} title={props.item[2].title} amount={props.item[2].amount} />
     <ExpenseItem date={props.item[3].date} title={props.item[3].title} amount={props.item[3].amount} />
-  </div>
+  </Card>
 }
 
 
